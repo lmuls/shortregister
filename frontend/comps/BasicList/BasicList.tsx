ee1d137430406017fc6caf05fb4050fdfdee2778
Lars Marius Ulsnes <lars.marius.ulsnes@soprasteria.com>
2022-05-31 16:03:33 +0200

    Worked on frontend

---
+++ frontend/comps/BasicList/BasicList.tsx
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import {types} from "util";
+import {JSXElement} from "@babel/types";
+
+type ListProps = {
+    data: [
+        instruments: {
+            isin: string;
+            issuerName: string;
+        }
+    ]
+}
+
+const BasicList = ({data} : ListProps): JSX.Element => {
+
+    function mapElements(element: {isin: string, issuerName: string}): JSX.Element {
+        return <ListItem disablePadding>
+            <ListItemButton>
+                <ListItemText primary={element.issuerName} />
+            </ListItemButton>
+        </ListItem>
+    }
+
+    return (
+        <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+            <nav aria-label="main mailbox folders">
+                <List>
+                    <ListItem disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <InboxIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Inbox" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <DraftsIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Drafts" />
+                        </ListItemButton>
+                    </ListItem>
+                </List>
+            </nav>
+            <Divider />
+            <nav aria-label="secondary mailbox folders">
+
+                <List>
+                    <ListItem disablePadding>
+                        <ListItemButton>
+                            <ListItemText primary="Trash" />
+                            <ListItemText primary="Trash" />
+                        </ListItemButton>
+                    </ListItem>
+                    {data.map((elem) => {
+                        return mapElements(elem);
+                    })}
+                </List>
+            </nav>
+        </Box>
+    );
+}
+
+export default BasicList;
---
+++ frontend/pages/instruments.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+const Instruments = (instruments: any): JSX.Element => {
+
+
+    return (<div>
+        <h2>Instruments component</h2>
+    </div>)
+}
+
+export default Instruments;
